7f094103181774208a8d6fed8f0c5d24507922ee
fghjok <fghjok@sina.com>
2019-10-31 15:09:57 +0800

    xx

---
+++ build/precache-manifest.62f3fe5fd755db6497a6dae030fe1982.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8354a0346a3450a2681733e9e6d71464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38137c5779fefee4c3a2",
+    "url": "/static/js/2.fab56cf6.chunk.js"
+  },
+  {
+    "revision": "cc8a37c36f9567f498b4",
+    "url": "/static/js/main.9f3d28c6.chunk.js"
+  },
+  {
+    "revision": "257861243e8eec05bb1d",
+    "url": "/static/js/runtime-main.aa49d4ae.js"
+  }
+]);
